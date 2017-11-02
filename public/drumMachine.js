1ca9e0ea0f87b04687686e85d4938dab2f1a2c4a
David-Bohl <davebohl@gmail.com>
2017-11-02 19:17:10 -0400

    add synth in same js file as drum machine.

--- public/drumMachine.js
+++ public/drumMachine.js
@@ -1,40 +1,42 @@
 Nexus.context = Tone.context
+
+
+
+output = [];
+
+
+
+/* ------------ Synth ------------ */
+var synth = new Tone.Synth().toMaster();
+
+
+
+
+
 var sequencer = new Nexus.Sequencer('#synth',{
   'size': [600,300],
   'mode': 'toggle',
-  'rows': 4,
+  'rows': 8,
   'columns': 16
 })
 sequencer.colorize("accent", "black");
 
-var kick = new Tone.Player({"url" : "/vendor/Samples/kick.wav", "fadeOut" : 0.2}).toMaster();
-var sistersnare = new Tone.Player({"url" : "/vendor/Samples/sistersnare.wav", "fadeOut" : 0.2}).toMaster();
-var hat = new Tone.Player({"url" : "/vendor/Samples/hat.wav", "fadeOut" : 0.2}).toMaster();
-var hihat = new Tone.Player({"url" : "/vendor/Samples/hihat.wav", "fadeOut" : 0.2}).toMaster();
-
-output = [];
 sequencer.start();
 
-var sequencer2 = new Nexus.Sequencer('#drums',{
-  'size': [600,150],
-  'mode': 'toggle',
-  'rows': 4,
-  'columns': 16
-})
-sequencer2.colorize("accent", "orange");
-
-sequencer2.start();
-//console.log(number.value * 100);
 sequencer.on('step',function(v) {
-  //console.log(v);
-  if (v[0] == 1){kick.start();}
-  if (v[1] == 1){sistersnare.start();}
-  if (v[2] == 1){hat.start();}
-  if (v[3] == 1){hihat.start();}
+  if (v[0] == 1){synth.triggerAttackRelease('C3', '16n');}
+  if (v[1] == 1){synth.triggerAttackRelease('D3', '16n');}
+  if (v[2] == 1){synth.triggerAttackRelease('E3', '16n');}
+  if (v[3] == 1){synth.triggerAttackRelease('F3', '16n');}
+  if (v[4] == 1){synth.triggerAttackRelease('G3', '16n');}
+  if (v[5] == 1){synth.triggerAttackRelease('A3', '16n');}
+  if (v[6] == 1){synth.triggerAttackRelease('B3', '16n');}
+  if (v[7] == 1){synth.triggerAttackRelease('C4', '16n');}
 
  output = v;
-
 })
+
+
 
 sequencer.on('change',function(v) {
     var socket = io();
@@ -47,10 +49,25 @@ sequencer.on('change',function(v) {
 });
 
 
-//console.log(number.value * 100);
-sequencer2.on('step',function(v) {
-  //console.log(v);
 
+/* ------------ Drums ------------ */
+
+var kick = new Tone.Player({"url" : "/vendor/Samples/kick.wav", "fadeOut" : 0.2}).toMaster();
+var sistersnare = new Tone.Player({"url" : "/vendor/Samples/sistersnare.wav", "fadeOut" : 0.2}).toMaster();
+var hat = new Tone.Player({"url" : "/vendor/Samples/hat.wav", "fadeOut" : 0.2}).toMaster();
+var hihat = new Tone.Player({"url" : "/vendor/Samples/hihat.wav", "fadeOut" : 0.2}).toMaster();
+
+var sequencer2 = new Nexus.Sequencer('#drums',{
+  'size': [600,150],
+  'mode': 'toggle',
+  'rows': 4,
+  'columns': 16
+})
+sequencer2.colorize("accent", "orange");
+
+sequencer2.start();
+
+sequencer2.on('step',function(v) {
   if (v[0] == 1){kick.start();}
   if (v[1] == 1){sistersnare.start();}
   if (v[2] == 1){hat.start();}
--- public/synth.js
+++ public/synth.js
@@ -1,1 +1,1 @@
-console.log("synth here");
+
