8ba2de5f71c71df16e7240def8643c301133ef61
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-11-11 02:12:07 -0500

    Sucessfully tested players for tone js

--- public/drumMachine.js
+++ public/drumMachine.js
@@ -2,33 +2,53 @@ Nexus.context = Tone.context
 output = [];
 
 /* ------------ Drums ------------ */
+// Used Tone.Players for optimization
 
-var kick = new Tone.Player({"url" : "/vendor/Samples/kick.wav", "fadeOut" : 0.2}).toMaster();
-var sistersnare = new Tone.Player({"url" : "/vendor/Samples/sistersnare.wav", "fadeOut" : 0.2}).toMaster();
-var hat = new Tone.Player({"url" : "/vendor/Samples/hat.wav", "fadeOut" : 0.2}).toMaster();
-var hihat = new Tone.Player({"url" : "/vendor/Samples/hihat.wav", "fadeOut" : 0.2}).toMaster();
+var drums = new Tone.Players({
+    "Kick": "/vendor/Samples/kick.wav",
+    "Sistersnare": "/vendor/Samples/sistersnare.wav",
+    "Hat": "/vendor/Samples/hat.wav",
+    "Hihat": "/vendor/Samples/hihat.wav",
+}, {
+    "volume": "10",
+    "fadeOut": "64n",
+}).toMaster();
 
-var sequencer2 = new Nexus.Sequencer('#drums',{
-  'size': [600,150],
-  'mode': 'toggle',
-  'rows': 4,
-  'columns': 16
+var sequencer2 = new Nexus.Sequencer('#drums', {
+    'size': [600, 150],
+    'mode': 'toggle',
+    'rows': 4,
+    'columns': 16
 })
+
+var notes = ["Kick", "Sistersnare", "Hat", "Hihat"];
+
 sequencer2.colorize("accent", "orange");
 
+var loop = new Tone.Sequence((time, col) => {
+    var column = sequencer2.matrix.column(col);
+    for (var i = 0; i < 4; i++) {
+        if (column[i]) {
+            //console.log("Got in");
+            var vel = Math.random() * 0.5 + 0.5;
+            drums.get(notes[i]).start(time, 0, "16n", 0, vel);
+        }
+    }
+}, [0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15], "16n");
+
+Tone.Transport.start();
 //sequencer2.start();
+loop.start();
 
-sequencer2.on('step',function(v) {
-  if (v[0] == 1){kick.start();}
-  if (v[1] == 1){sistersnare.start();}
-  if (v[2] == 1){hat.start();}
-  if (v[3] == 1){hihat.start();}
+Tone.Transport.bpm.value = 100;
+// sequencer2.on('step', function(v) {
+//     if (v[0] == 1) { kick.start(); }
+//     if (v[1] == 1) { sistersnare.start(); }
+//     if (v[2] == 1) { hat.start(); }
+//     if (v[3] == 1) { hihat.start(); }
 
- output = v;
-
-});
-
-
+//     output = v;
+// });
 
 // sequencer2.on('change',function(v) {
 //     var socket = io();
--- public/globalControls.js
+++ public/globalControls.js
@@ -3,16 +3,16 @@ seqs = [];
 var isRunning = false;
 
 function start_stop(event) {
-  for(seq of seqs) {
-    if(!isRunning)
-      seq.start();
-    else {
-      seq.stop();
-      seq.stepper.value = 15;
+    for (seq of seqs) {
+        if (!isRunning)
+            seq.start();
+        else {
+            seq.stop();
+            seq.stepper.value = 15;
+        }
     }
-  }
 
-  isRunning = !isRunning;
+    isRunning = !isRunning;
 }
 
 document.getElementById('start_stop_btn').addEventListener('click', start_stop);
