b48caa7819ff08efea24fe8345a60afd93859825
Rupesh Basnet <rupeshbasnet93@gmail.com>
2017-11-12 01:48:53 -0500

    Added routes and views for signup and login

--- controllers/index.js
+++ controllers/index.js
@@ -2,8 +2,9 @@ const express = require('express');
 const router = express.Router();
 
 
-router.use('/alt', require('./alt'));
-router.use('/', require('./home'));
+router.use('/login', require('./login'));
+router.use('/signup', require('./signup'));
+router.use('/profile', require('./profile'));
 
 
 router.get('/', (req, res) => {
---
+++ models/user.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const bcrypt = require('bcrypt-nodejs');
+
+module.exports = (sequelize, DataTypes) => {
+  const User = sequelize.define('user', {
+    firstName: DataTypes.STRING,
+    lastName: DataTypes.STRING,
+    username: DataTypes.STRING,
+    email: DataTypes.STRING,
+    password_hash: DataTypes.STRING,
+    password: DataTypes.VIRTUAL,
+  });
+
+  User.beforeCreate((user) =>
+    new sequelize.Promise((resolve) => {
+      bcrypt.hash(user.password, null, null, (err, hashedPassword) => {
+        resolve(hashedPassword);
+      });
+    }).then((hashedPassword) => {
+      user.password = hashedPassword;
+    })
+  );
+
+  return User;
+};
+
