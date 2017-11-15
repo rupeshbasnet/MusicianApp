882b093742219fd62cc356da3bad3b47076f4048
David-Bohl <davebohl@gmail.com>
2017-11-15 13:35:49 -0500

    add back in render homepage

--- controllers/index.js
+++ controllers/index.js
@@ -5,7 +5,9 @@ const router = express.Router();
 router.use('/users', require('./users'));
 router.use('/', require('./home'));
 
-
+router.get('/', (req, res) => {
+  res.render('homepage');
+});
 
 
 module.exports = router;
