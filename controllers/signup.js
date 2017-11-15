80cd513a3bcdaf5d651f477452e1ac75bfc1773c
David-Bohl <davebohl@gmail.com>
2017-11-15 16:40:04 -0500

    user can signup from signup page

--- controllers/signup.js
+++ controllers/signup.js
@@ -6,20 +6,34 @@ router.get('/', (req, res) => {
   res.render('signup')
 });
 
+// router.post('/', (req, res) => {
+//   User.create({
+//     firstName: req.body.firstName,
+//     lastName: req.body.lastName,
+//     username: req.body.username,
+//     email: req.body.email,
+//     password: req.body.password,
+//   }).then((user) => {
+//     req.login(user, () =>
+//       res.redirect('/profile')
+//     );
+//   }).catch(() => {
+//     res.render('signup');
+//   });
+// });
+
+// Create a new user
 router.post('/', (req, res) => {
-  User.create({
-    firstName: req.body.firstName,
-    lastName: req.body.lastName,
+  models.Users.create({
     username: req.body.username,
-    email: req.body.email,
-    password: req.body.password,
-  }).then((user) => {
-    req.login(user, () =>
-      res.redirect('/profile')
-    );
-  }).catch(() => {
-    res.render('signup');
-  });
+    password: req.body.password
+  })
+  .then((users) => {
+    res.json(users);
+  })
+  .catch(() => {
+    res.sendStatus(400);
+  })
 });
 
 module.exports = router;
