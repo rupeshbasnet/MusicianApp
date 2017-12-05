434998685a63d52f418562bd0bc1163512d7b457
Frederick Erlikh <erlikhf@gmail.com>
2017-12-04 21:42:12 -0500

    Consolidated socket controls. pattern switching

--- public/drumMachine.js
+++ public/drumMachine.js
@@ -38,30 +38,3 @@ function loopDrum(time, col) {
         }
     }
 }
-
-// drumSequencer.on('change',function(v) {
-//     var socket = io();
-//     socket.emit('beat2', drumSequencer.matrix.pattern);
-//     //console.log(sequencer.matrix.pattern);
-//     socket.on('beat2', function( data ) {
-//       drumSequencer.matrix.set.all(data);
-//       console.log(data);
-//     });
-// });
-
-var socket = io();
-socket.on('drums', function( data ) {
-  drumSequencer.matrix.set.all(data);
-});
-
-function drumsEmit() {
-  socket.emit('drums', drumSequencer.matrix.pattern);
-}
-
-for(let cell of $('#drums div').children())
-{
-  cell.addEventListener('mouseup', drumsEmit);
-  cell.addEventListener('touchend', drumsEmit);
-}
-
-$(document).on('mouseup', drumsEmit);
--- public/globalControls.js
+++ public/globalControls.js
@@ -27,26 +27,6 @@ slider.on('change', function(v) {
     //tempoEmit();
 });
 
-//send tempo over socket
-var socket = io();
-socket.on('tempo', function( data ) {
-  slider.value = parseInt(data);
-  //console.log(data);
-});
-
-function tempoEmit(){
-  socket.emit('tempo', slider.value);
-}
-
-// for Desktop, hacky
-$('svg')[0].addEventListener('mousemove', function(e){
-  if(e.buttons == 1)
-    window.addEventListener('mousemove', tempoEmit, { once: true });
-});
-
-// Mobile
-$('svg')[0].addEventListener('touchmove', tempoEmit);
-
 var textbutton = new Nexus.TextButton('#button', {
     'size': [150, 50],
     'state': false,
