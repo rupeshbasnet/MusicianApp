0beb921f0107a02aa7c36b59a64495015287dff2
Frederick Erlikh <erlikhf@gmail.com>
2017-12-06 18:40:50 -0500

    Tuning socket controls

--- public/socketDrums.js
+++ public/socketDrums.js
@@ -12,7 +12,15 @@ for(let cell of $('#drums div').children())
   cell.addEventListener('touchend', drumsEmit);
 }
 
-$(document).on('mouseup', drumsEmit);
+$('#drums div').mouseleave(function(e){
+
+  if(e.buttons === 1)
+    $(document).on('mouseup', function(){
+      drumsEmit();
+      $(document).off('mouseup');
+    });
+
+});
 
 socket.on('drums', function( data ) {
   drumSequencer.matrix.set.all(data);
